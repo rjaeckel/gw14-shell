24beae80c6ed7c5c53db9ec613bb5cc30c96dbec
Robert Jäckel <robert.jaeckel@itz.uni-halle.de>
2015-11-20 16:42:38 +0100

    generic version

--- crons/export_agent_configs.php
+++ crons/export_agent_configs.php
@@ -15,8 +15,30 @@ require('application.php');
 //  create output directory, if neccessary, suppress warnings by @-prefix
 @mkdir($outputDir,0750,true);
 
+
+$agents=apiResult::merge(array(
+	'MTA'=>'domain',
+	'POA'=>'postOffice',
+	'GWIA'=>'domain'
+ ))->each(function($match,$type)use($outputDir){
+	$fn=$type.'s';
+	$fn()->each(function($obj)use($outputDir,$match,$type){
+		/** @var $obj apiResult */
+		$fname=implode('.',array(
+			$obj($match.'Name'),
+			$type,
+			'cfg'
+		));
+		$cfg = (array)$obj->content;
+		ksort($cfg);
+		file_put_contents($outputDir.DIRECTORY_SEPARATOR.$fName,print_r($cfg,1));
+	});
+});
+
+/*
+
 Mtas()->each(function($mta)use($outputDir){
-	/** @var mta|apiResult $mta */
+	/** @var mta|apiResult $mta * /
 	$fName=$mta->domainName.'.MTA.cfg';
 
 	$cfg = (array)$mta->content;
@@ -27,7 +49,7 @@ Mtas()->each(function($mta)use($outputDir){
 
 
 Poas()->each(function($poa)use($outputDir){
-	/** @var poa|apiResult $poa */
+	/** @var poa|apiResult $poa * /
 	$fName=$poa->postOfficeName.'.POA.cfg';
 
 	$cfg = (array)$poa->content;
@@ -37,7 +59,7 @@ Poas()->each(function($poa)use($outputDir){
 });
 
 Gwias()->each(function($gwia)use($outputDir){
-	/** @var gwia|apiResult $gwia */
+	/** @var gwia|apiResult $gwia * /
 	$fName=$gwia->domainName.'.GWIA.cfg';
 
 	$cfg = (array)$gwia->content;
@@ -45,3 +67,5 @@ Gwias()->each(function($gwia)use($outputDir){
 
 	file_put_contents($outputDir.DIRECTORY_SEPARATOR.$fName,print_r($cfg,1));
 });
+
+*/
