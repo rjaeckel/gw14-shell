9eeb8c1f2a4b52c8cb26a84379669017b18683d3
Robert Jäckel <robert.jaeckel@itz.uni-halle.de>
2015-11-25 14:34:34 +0100

    bugfix for generated "object" class. This is a keyword in php, this class is available as "obj" now

--- api-gen/wadl-abstract.php
+++ api-gen/wadl-abstract.php
@@ -581,11 +581,11 @@ function getFilledNodes($list,$level=0) {
             foreach($keys as $name) {
                 $myMethods[cleanUpMethodName($name)?:$name]=$methods->$name;
             }
-            $myMethods['__className']=$className=findClassName($myMethods, array('update','create','get'), function($m){
+            $myMethods['__className']=($className=findClassName($myMethods, array('update','create','get'), function($m){
                 return $m->requestType?:$m->responseType?:false;
                 })?:(($c=count(urlStack()))>2?
                         urlStack($c-2)
-                        :urlStack(0));
+                        :urlStack(0)))=='object'?'obj':$className;
             ksort($myMethods);
             mergeAndReduce(array($className=>(object)$myMethods), $res);
         }
