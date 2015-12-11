076acd71861771193ce886011fdd600004105e97
rjaeckel <robert.jaeckel@itz.uni-halle.de>
2015-12-11 09:03:46 +0100

    gw14.0.2 xsd tree

--- classes/mlu.groupwise.xsd/verifyFlag.cls.php
+++ classes/mlu.groupwise.xsd/verifyFlag.cls.php
@@ -4,6 +4,7 @@ namespace mlu\groupwise\xsd;
   * XSD-abstracted interfaces...
   */
 abstract class verifyFlag extends \mlu\rest\xsd_restriction { 
+	const NONE='NONE';
 	const STRUCTURE='STRUCTURE';
 	const CONTENTS='CONTENTS';
 }
