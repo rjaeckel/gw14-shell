67d535fae94ec66419f95616ac52f28296f2612f
rjaeckel <robert.jaeckel@itz.uni-halle.de>
2016-10-21 12:04:28 +0200

    fix for non console output

--- common/functions/logWrite.fn.php
+++ common/functions/logWrite.fn.php
@@ -6,7 +6,7 @@ function logWrite($msg,$target=STDOUT,$eol="\r") {
 	global $argv;
 	$cols = ((int)`if [ -t 0 ]; then tput cols; fi`);
 	$prefix=sprintf("[%s %s] ", date("d.m.Y-H:i:s"), basename($argv[0],'.php'));
-	$line = sprintf("%-{$cols}.{$cols}s",$prefix.$msg);
+	$line = $cols?sprintf("%-{$cols}.{$cols}s",$prefix.$msg):($prefix.$msg);
 	return fwrite($target,$line.$eol);
 }
 
