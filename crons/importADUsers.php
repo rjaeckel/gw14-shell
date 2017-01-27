1ee5e4b89191255617bc839c17298360645670d7
rjaeckel <robert.jaeckel@itz.uni-halle.de>
2017-01-27 16:45:23 +0100

    DLM: Import nur wenn comment=active (Redmine #772)

--- crons/importADUsers.php
+++ crons/importADUsers.php
@@ -171,7 +171,7 @@ function importUsers($tempPOId) {
         @showFailed=>true,
         @showSkipped=>false,
         @showSucceeded=>false,
-	@filter=>'(&(objectClass=user)(postOfficeBox=*@*)(!(postOfficeBox=*@informatik*))(memberof:1.2.840.113556.1.4.1941:=CN=MLU E-Mail zentral,OU=MLU Global,OU=Uni-Halle,DC=xd,DC=uni-halle,DC=de))'
+	@filter=>'(&(objectClass=user)(comment=active)(postOfficeBox=*@*)(!(postOfficeBox=*@informatik*))(memberof:0.2.840.113556.1.4.1941:=CN=MLU E-Mail zentral,OU=MLU Global,OU=Uni-Halle,DC=xd,DC=uni-halle,DC=de))'
 	//@filter=>'(&(objectClass=user)(memberof:1.2.840.113556.1.4.1941:=CN=MLU E-Mail zentral,OU=MLU Global,OU=Uni-Halle,DC=xd,DC=uni-halle,DC=de))'
     );
 // find new users in ldap using config above
